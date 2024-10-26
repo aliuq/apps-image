8747df63059c61d8795e9e71a2929a5478d33472
aliuq <aliuq@bilishare.com>
2024-10-26 17:32:28 +0800

    chore(filecodebox-web): add Dockerfile and entrypoint script for web application

---
+++ app/filecodebox-web/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+ME=$(basename "$0")
+
+entrypoint_log() {
+  if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    echo "$@"
+  fi
+}
+
+auto_envsubst() {
+  local filter="${NGINX_ENVSUBST_FILTER:-}"
+
+  defined_envs=$(printf '${%s} ' $(awk "END { for (name in ENVIRON) { print ( name ~ /${filter}/ ) ? name : \"\" } }" </dev/null))
+
+  entrypoint_log "$ME: Replacing……"
+  find "/app" -follow -type f -name "*.js" -print | while read -r template; do
+    envsubst "$defined_envs" <"$template" >"$template.1"
+    mv "$template.1" "$template"
+  done
+}
+
+auto_envsubst
+
+exit 0
