2c18cc1404958636765bb83b62703d68fcb8cc4e
aliuq <aliuq@bilishare.com>
2024-10-26 16:39:06 +0800

    chore(nginx): add nginx

---
+++ base/nginx/Dockerfile.vue
@@ -0,0 +1,11 @@
+FROM nginx:alpine
+
+LABEL version="0.0.1"
+LABEL maintainer="AliuQ <aliuq@bilishare.com>"
+LABEL description="Nginx with custom configuration, based on alpine. The root path is /app."
+
+WORKDIR /app
+COPY ./conf/default.vue.conf /etc/nginx/conf.d/default.conf
+CMD ["nginx", "-g", "daemon off;"]
+
+EXPOSE 80
