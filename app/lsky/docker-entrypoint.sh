c6f1773bc6b2f0447d814d9ddb391669c6fcb614
aliuq <aliuq@bilishare.com>
2024-11-05 23:01:23 +0800

    chore(lsky): add Lsky

---
+++ app/lsky/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ME=$(basename "$0")
+
+if [ ! -z "$UPLOAD_MAX_FILESIZE" ]; then
+    sed -i "s/upload_max_filesize = .*/upload_max_filesize = $UPLOAD_MAX_FILESIZE/g" $PHP_INI_DIR/php.ini
+    echo "$ME: Set upload_max_filesize to $UPLOAD_MAX_FILESIZE"
+fi
+
+if [ ! -z "$POST_MAX_SIZE" ]; then
+    sed -i "s/post_max_size = .*/post_max_size = $POST_MAX_SIZE/g" $PHP_INI_DIR/php.ini
+    echo "$ME: Set post_max_size to $POST_MAX_SIZE"
+fi
+
+if [ ! -d "/app/vendor" ]; then
+    composer install
+    echo "\n$ME: Installed composer dependencies"
+fi
+
+php artisan serve --host=0.0.0.0 --port=80
