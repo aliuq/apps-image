e1d53844e5fb6c018ac0441355031f826662c064
aliuq <aliuq@bilishare.com>
2024-11-08 06:49:52 +0800

    chore(nginx): add svelte nginx

---
+++ base/nginx/Dockerfile.svelte
@@ -0,0 +1,12 @@
+FROM nginx:alpine
+
+LABEL version="0.0.1"
+LABEL maintainer="AliuQ <aliuq@bilishare.com>"
+LABEL description="Nginx with custom configuration, based on alpine. The root path is /app."
+
+WORKDIR /app
+COPY ./conf/default.svelte.conf /etc/nginx/conf.d/default.conf
+
+CMD ["nginx", "-g", "daemon off;"]
+
+EXPOSE 80
