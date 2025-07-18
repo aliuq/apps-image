cfa42076142ad70a1d83d8c11482989b9606b068
aliuq <aliuq@bilishare.com>
2025-07-18 17:57:31 +0800

    chore(cobalt): update Dockerfile and docker-compose for improved build process and version bump

--- apps/cobalt/docker-entrypoint.sh
+++ apps/cobalt/docker-entrypoint.sh
@@ -15,6 +15,10 @@ auto_envsubst() {
 
   defined_envs=$(printf '${%s} ' $(awk "END { for (name in ENVIRON) { print ( name ~ /${filter}/ ) ? name : \"\" } }" </dev/null))
 
+  # 打印调试信息
+  # entrypoint_log "$ME: Filter pattern: '$filter'"
+  # entrypoint_log "$ME: Defined environment variables: $defined_envs"
+
   entrypoint_log "$ME: Replacing……"
   find "/app" -follow -type f -name "*.js" -print | while read -r template; do
     envsubst "$defined_envs" <"$template" >"$template.1"
