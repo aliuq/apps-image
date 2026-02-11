949a61e42366a0e101c88395f31ba56574e1ae40
aliuq <aliuq@bilishare.com>
2026-02-11 10:16:20 +0000

    cobalt: dev variant will watch source `web` path

---
+++ apps/cobalt/pre.dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+VERSION="{{version}}"
+
+# Clone the repository
+mkdir -p app && cd app
+git clone https://github.com/imputnet/cobalt . && git checkout $VERSION
+rm -rf .git
+
+mise use pnpm -g
+corepack enable && corepack prepare --activate
+pnpm install --frozen-lockfile
+WEB_DEFAULT_API=\$BASE_API pnpm -C web build
+
+mise cache clear
+rm -rf node_modules .next/server
